b22b54c608e3410b0b864e33e9355920226184f9
Luiz Carlos Ferreira <84511085+luizferreira7@users.noreply.github.com>
2024-03-10 01:13:36 -0300

    Adiciona mensagem para exibir status de salvamento de vagas

--- src/main/java/com/teste/pratico/bean/VagasManagedBean.java
+++ src/main/java/com/teste/pratico/bean/VagasManagedBean.java
@@ -5,9 +5,10 @@ import com.teste.pratico.service.VagasService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.ManagedBean;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import java.util.Date;
-import java.util.List;
 
 @ManagedBean
 @ViewScoped
@@ -20,12 +21,13 @@ public class VagasManagedBean {
 
     private Date dataAtual = new Date();
 
-    public void salvarVagas() {
-        vagasService.criaNovasVagas(vagasVO);
+    public void info() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "Vagas cadastradas."));
     }
 
-    public List<VagasVO> getVagasAtivas() {
-        return vagasService.recuperaTodasVagasAtivas();
+    public void salvarVagas() {
+        vagasService.criaNovasVagas(vagasVO);
+        info();
     }
 
     public VagasVO getVagasVO() {
