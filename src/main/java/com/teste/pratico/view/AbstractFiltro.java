7c46cd5d6206867f20aa266d5d6429459cc96769
Luiz Carlos Ferreira <84511085+luizferreira7@users.noreply.github.com>
2024-03-11 18:46:25 -0300

    Cria layout para modal form

--- src/main/java/com/teste/pratico/view/AbstractFiltro.java
+++ src/main/java/com/teste/pratico/view/AbstractFiltro.java
@@ -19,7 +19,7 @@ public abstract class AbstractFiltro<T> {
     public abstract void atualizaResultado();
 
     public void notfound() {
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Consulta", "Não foi encontrado resultado para sua consulta"));
+        FacesContext.getCurrentInstance().addMessage("messages", new FacesMessage(FacesMessage.SEVERITY_WARN, "Consulta", "Não foi encontrado resultado para sua consulta"));
     }
 
     public String cadastrar() {
